44dfeaab4edf852993956a36d81acfb068aabcfc
muhammadyusronn <muhammadyusron733@gmail.com>
2021-09-04 03:12:14 +0700

    init app

---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,112 @@
+<!-- sidebar  -->
+<nav class="sidebar">
+    <div class="logo d-flex justify-content-between">
+        <a class="large_logo" href="#"><img src="{{asset('assets/img/logo_uptpkk.png')}}" alt=""></a>
+        <a class="small_logo" href="#"><img src="{{asset('assets/img/mini_logo.png')}}" alt=""></a>
+        <div class="sidebar_close_icon d-lg-none">
+            <i class="ti-close"></i>
+        </div>
+    </div>
+    <ul id="sidebar_menu">
+        <li class="">
+            <a href="#" aria-expanded="false">
+                <div class="nav_icon_small">
+                    <img src="{{asset('assets/img/menu-icon/research.svg')}}" alt="">
+                </div>
+                <div class="nav_title">
+                    <span>Lowongan Terbaru </span>
+                </div>
+            </a>
+        </li>
+        <li class="">
+            <a href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/newspaper.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Berita Terbaru</span>
+            </div>
+            </a>
+        </li>
+        <li class="">
+            <a href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/promotion.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Pengumuman Terbaru</span>
+            </div>
+            </a>
+        </li>
+        <li class="">
+            <a href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/search.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Latar Belakang</span>
+            </div>
+            </a>
+        </li>
+        <li class="">
+            <a href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/idea.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Visi, Misi & Tujuan</span>
+            </div>
+            </a>
+        </li>
+        <li class="">
+            <a href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/management.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Struktur Organisasi</span>
+            </div>
+            </a>
+        </li>
+        <li class="">
+            <a class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/blogger.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Karir</span>
+            </div>
+            </a>
+
+            <ul>
+                <li><a href="">
+                    Seminar, Training & <br>Workshop</a>
+                </li>
+                <li>
+                    <a href="">Konseling</a>
+                </li>
+            </ul>
+        </li>
+        <li class="">
+            <a class="has-arrow" href="#" aria-expanded="false">
+              <div class="nav_icon_small">
+                <img src="{{('assets/img/menu-icon/people-trading.svg')}}" alt="">
+            </div>
+            <div class="nav_title">
+                <span>Kewirausahan</span>
+            </div>
+            </a>
+
+            <ul>
+                <li><a href="">Kewirausahan</a></li>
+                <li><a href="">
+                    Seminar, Training & <br>Workshop</a>
+                </li>
+                <li>
+                    <a href="">Konseling</a>
+                </li>
+            </ul>
+        </li>
+      </ul>
+</nav>
+ <!--/ sidebar  -->
