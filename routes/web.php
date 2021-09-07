3a2c93fe6577ed0651b32a1436c77bf36dda2483
rosaalwyh <alawiyahrosa@gmail.com>
2021-09-08 00:24:51 +0700

    add template frontend

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\backend\AdminController;
 use App\Http\Controllers\backend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'layouts.admin.app')->name('home');
+Route::view('/', 'frontend.app')->name('home');
 
 Route::get('/dash', [HomeController::class, 'index']);
 // Route::get('/dash', [AdminController::class, 'index']);
