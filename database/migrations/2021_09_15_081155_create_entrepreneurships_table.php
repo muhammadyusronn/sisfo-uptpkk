f4853408b061eb1a9b107c810ea789b7be4d8425
muhammadyusronn <muhammadyusron733@gmail.com>
2021-09-15 15:12:34 +0700

    Created : migration for kewirausahaan

---
+++ database/migrations/2021_09_15_081155_create_entrepreneurships_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntrepreneurshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrepreneurships', function (Blueprint $table) {
+            $table->bigIncrements('kewirausahaan_id');
+            $table->string('kewirausahaan_judul');
+            $table->text('kewirausahaan_konten');
+            $table->string('kewirausahaan_foto');
+            $table->unsignedBigInteger('kewirausahaan_author');
+            $table->boolean('kewirausahaan_status');
+            $table->timestamps();
+            $table->foreign('kewirausahaan_author')->references('user_id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrepreneurships');
+    }
+}
